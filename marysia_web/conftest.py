fe3b541cee4075e27b86365440fa5d7d48162d68
Adam Beret Manczuk <beret@hipisi.org.pl>
2021-12-26 20:44:15 +0000

    empty Django project

---
+++ marysia_web/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from marysia_web.users.models import User
+from marysia_web.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
